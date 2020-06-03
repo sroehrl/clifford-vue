1c09729814b805f9c324f6d32afa7621aaff29e7
sroehrl <stefan.roehrl@gmail.com>
2020-06-03 18:09:34 -0400

    starter vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <input type="text" v-model="zip">
+      <button @click="askBackend">check zip-code</button>
+    </div>
+    <div v-if="dealerFound">
+      <h2>Your dealer: {{dealer.name}}</h2>
+      <p>located at {{dealer.postal_code}}</p>
+    </div>
+    <p v-if="warning">
+      NO DEALER
+    </p>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios'
 
 export default {
   name: 'Home',
+  data:function(){
+    return {
+      zip:'',
+      dealerFound: false,
+      warning:false,
+      dealer:{}
+    }
+  },
+  methods:{
+    async askBackend(){
+      try{
+        let beZip = await axios.get('http://localhost/clifford-api/api.v1/zip?zip='+this.zip)
+        if(beZip.data.length > 0){
+          this.dealerFound = true;
+          this.dealer = beZip.data[0];
+        } else {
+          this.warning = true;
+        }
+      } catch (e) {
+        alert('backend unavailable')
+      }
+
+
+    }
+  },
   components: {
     HelloWorld
   }
